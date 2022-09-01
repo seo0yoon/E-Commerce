e4558deb2805c0190726dbbb6845db31c6a968a1
Yoon seo yeong <genious921225@gmail.com>
2022-09-01 18:35:55 +0900

    Update: Nav add

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,27 @@
 import React from "react";
+import "./Nav.scss";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <header className="nav">
+      <div className="navWrap">
+        <div className="navLogo">
+          <img src="/images/Logo.svg" alt="logo" />
+        </div>
+        <ul className="navMenu">
+          <li>Home</li>
+          <li>Shop</li>
+          <li>About</li>
+          <li>Contacts</li>
+          <li>Journal</li>
+        </ul>
+        <ul className="navLang">
+          <li>EN</li>
+          <li>RU</li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Nav;
