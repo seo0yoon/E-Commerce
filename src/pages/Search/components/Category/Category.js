4774571153ceba33bd3332d6114028532ff807d1
Yoon seo yeong <genious921225@gmail.com>
2022-09-06 19:57:47 +0900

    Update

--- src/pages/Search/components/Category/Category.js
+++ src/pages/Search/components/Category/Category.js
@@ -1,13 +1,10 @@
-import React from 'react';
+import React from "react";
 
 function Category() {
   return (
     <div className="category">
       <div className="category-icon">
-        <img
-          src="/assets/search/category_menu-icon.svg"
-          alt="카테고리로 이동"
-        />
+        <img src="/assets/search/category_menu-icon.svg" alt="카테고리로 이동" />
       </div>
       <div className="context">Shop by Category</div>
     </div>
