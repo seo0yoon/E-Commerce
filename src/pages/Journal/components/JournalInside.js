796336ed193e3c5c9ccb7c2cdc08045018b470f6
Yoon seo yeong <genious921225@gmail.com>
2022-09-06 16:46:59 +0900

    Update: style 수정

--- src/pages/Journal/components/JournalInside.js
+++ src/pages/Journal/components/JournalInside.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import JournalInput from './JournalInput';
-import RecentPosts from './RecentPosts';
-import { useSelector } from 'react-redux';
-import Archives from './Archives';
-import Categories from './Categories';
-import Metas from './Metas';
-import '../css/index.css';
+import React from "react";
+import JournalInput from "./JournalInput";
+import RecentPosts from "./RecentPosts";
+import { useSelector } from "react-redux";
+import Archives from "./Archives";
+import Categories from "./Categories";
+import Metas from "./Metas";
+import "../Journal.scss";
 
 function JournalInside() {
   // const articles = useSelector((state) => state.layouts.journal.articles);
@@ -27,26 +27,19 @@ function JournalInside() {
             <div className="router">Admin - Comments (1) - Category</div>
             <div className="desc">
               <div className="title txt-bold">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
               </div>
               <div className="context">
-                On the other hand, we denounce with righteous indignation and
-                dislike men who are so beguiled and demoralized by the charms of
-                pleasure of the moment, so blinded by desire, that they cannot
-                foresee the pain and trouble that are bound to ensue; and equal
-                blame belongs to those who fail in their duty through weakness
-                of will, which is the same as saying through shrinking from toil
-                and pain. These cases are perfectly simple and easy to
-                distinguish. In a free hour, when our power of choice is
-                untrammelled and when nothing prevents our being able to do what
-                we like best, every pleasure is to be welcomed and every pain
-                avoided. But in certain circumstances and owing to the claims of
-                duty or the obligations of business it will frequently occur
-                that pleasures have to be repudiated and annoyances accepted.
-                The wise man therefore always holds in these matters to this
-                principle of selection: he rejects pleasures to secure other
-                greater pleasures.
+                On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and
+                demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the
+                pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty
+                through weakness of will, which is the same as saying through shrinking from toil and pain. These cases
+                are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled
+                and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and
+                every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of
+                business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise
+                man therefore always holds in these matters to this principle of selection: he rejects pleasures to
+                secure other greater pleasures.
               </div>
             </div>
             <div className="tags txt-bold">
@@ -61,21 +54,16 @@ function JournalInside() {
                 <div className="date">6 months ago</div>
               </div>
               <div className="context">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry's standard dummy
-                text ever since the 1500s, when an unknown printer took a galley
-                of type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged.
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+                scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap
+                into electronic typesetting, remaining essentially unchanged.
               </div>
               <div className="reply txt-bold">Reply &gt;</div>
             </div>
             <div className="reply-block">
               <div className="title txt-bold">Leave a Reply</div>
-              <div className="guide">
-                Your email address will not be published. Required fields are
-                marked *
-              </div>
+              <div className="guide">Your email address will not be published. Required fields are marked *</div>
               <ul className="input-boxes">
                 <li className="input-box">
                   <div className="title txt-bold">Name</div>
@@ -98,8 +86,7 @@ function JournalInside() {
               <div className="save-check">
                 <div className="check-box"></div>
                 <div className="context txt-bold">
-                  Save my name, email, website in this browser for the next time
-                  I comment.
+                  Save my name, email, website in this browser for the next time I comment.
                 </div>
               </div>
               <div className="submit-btn txt-bold">Post</div>
@@ -112,13 +99,7 @@ function JournalInside() {
               <div className="title txt-bold">Recent Posts</div>
               <ul className="wrapper">
                 {recent_posts.map(({ front_txt, behind_txt }, idx) => {
-                  return (
-                    <RecentPosts
-                      front_txt={front_txt}
-                      behind_txt={behind_txt}
-                      key={idx}
-                    />
-                  );
+                  return <RecentPosts front_txt={front_txt} behind_txt={behind_txt} key={idx} />;
                 })}
               </ul>
             </div>
