39728612abf18d601831fb4a077ffb8e2f1b9331
Yoon seo yeong <genious921225@gmail.com>
2022-09-06 15:21:23 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "../src/components/Header/Header";
-import Search from "../src/components/Search/Search";
-import NotFound from "../src/components/NotFound/NotFound";
-import Footer from "../src/components/Footer/Footer";
-import About from "../src/components/About/About";
-import Home from "../src/components/Home/Home";
-import Shop from "../src/components/Shop/Shop";
-import Contacts from "../src/components/Contacts/Contacts";
-import Journal from "../src/components/Journal/Journal";
-import JournalInside from "../src/components/Journal/components/JournalInside";
-import ShopInside from "../src/components/ShopInside/ShopInside";
-import Cart from "../src/components/Cart/Cart";
-import MyAccount from "../src/components/MyAccount/MyAccount";
-import Login from "../src/components/Login/Login";
-import Signup from "../src/components/Signup/Signup";
+import Header from "../src/pages/Header/Header";
+import Search from "../src/pages/Search/Search";
+import NotFound from "../src/pages/NotFound/NotFound";
+import Footer from "../src/pages/Footer/Footer";
+import About from "../src/pages/About/About";
+import Home from "../src/pages/Home/Home";
+import Shop from "../src/pages/Shop/Shop";
+import Contacts from "../src/pages/Contacts/Contacts";
+import Journal from "../src/pages/Journal/Journal";
+import JournalInside from "../src/pages/Journal/components/JournalInside";
+import ShopInside from "../src/pages/ShopInside/ShopInside";
+import Cart from "../src/pages/Cart/Cart";
+import MyAccount from "../src/pages/MyAccount/MyAccount";
+import Login from "../src/pages/Login/Login";
+import Signup from "../src/pages/Signup/Signup";
 
 function App() {
   return (
---
+++ src/pages/Header/Header.js
@@ -0,0 +1,49 @@
+import React, { useCallback, useState } from "react";
+import Logo from "./components/Logo/Logo";
+import Navigation from "./components/Navigation/Navigation";
+import { useSelector } from "react-redux";
+import "./Header.scss";
+
+function Header() {
+  const menuSet = useSelector((state) => state.layouts.menus);
+
+  const [isEnClicked, setIsEnClicked] = useState(true);
+  const [isRuClicked, setIsRuClicked] = useState(false);
+
+  const selectEn = useCallback(() => {
+    setIsEnClicked(true);
+    setIsRuClicked(false);
+  }, []);
+
+  const selectRu = useCallback(() => {
+    setIsRuClicked(true);
+    setIsEnClicked(false);
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="wrapper">
+        <Logo menus={menuSet} />
+        <Navigation menus={menuSet} />
+        <ul className="languages">
+          <li
+            className={`language ${isEnClicked ? "clicked" : ""}
+                  ${isRuClicked ? "" : "clicked"}`}
+            onClick={selectEn}
+          >
+            EN
+          </li>
+          <li
+            className={`language ${isRuClicked ? "clicked" : ""}
+                  ${isEnClicked ? "" : "clicked"}`}
+            onClick={selectRu}
+          >
+            RU
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
