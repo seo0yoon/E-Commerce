849010b09309679ea5be783b060c3ab85a486563
Yoon seo yeong <genious921225@gmail.com>
2022-09-08 17:40:19 +0900

    Update

--- src/pages/Footer/components/Socials/Socials.js
+++ src/pages/Footer/components/Socials/Socials.js
@@ -5,13 +5,13 @@ const Socials = () => {
   return (
     <div className="socials">
       <div className="facebook-icon">
-        <img src="/images/Footer/facebookicon.png" alt="페이스북으로 가기" />
+        <img src="/images/footer/facebookicon.png" alt="페이스북으로 가기" />
       </div>
       <div className="in-icon">
-        <img src="/images/Footer/inicon.png" alt="in?으로 가기" />
+        <img src="/images/footer/inicon.png" alt="in?으로 가기" />
       </div>
       <div className="instagram-icon">
-        <img src="/images/Footer/instagramicon.png" alt="인스타그램으로 가기" />
+        <img src="/images/footer/instagramicon.png" alt="인스타그램으로 가기" />
       </div>
     </div>
   );
