fd21dedfc82d4f7d55938b9be838476ed2f0b41f
Yoon seo yeong <genious921225@gmail.com>
2022-09-08 17:39:48 +0900

    Update

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
         </div>
         <div className="right">
           <div className="logo">
-            <img src="/images/Footer/logo.png" alt="OREBI" />
+            <img src="/images/footer/logo.png" alt="OREBI" />
           </div>
           <div className="rights">2020 Orebi Minimal eCommerce Adobe XD Template by Adveits</div>
         </div>
